575b11c22774acf5a7277a30e9699f2773cf8df4
dnbaker <dnb@cs.jhu.edu>
2017-10-21 16:00:45 -0400

    Simplify.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # gfrp
 Generic Fast Randomized Projections
+
+
+## Third Party
+|Dependency | Reference | Comments |
+|-|-|-|
+|[Blaze](https://bitbucket.org/blaze-lib)|[K. Iglberger, et al.: Expression Templates Revisited: A Performance Analysis of Current Methodologies. SIAM Journal on Scientific Computing, 34(2): C42--C69, 2012](http://epubs.siam.org/sisc/resource/1/sjoce3/v34/i2/pC42_s1)|For optimal performance, this should be linked against BLAS and parallelized, as controlled in blaze/blaze/config/BLAS.h|
+|C++17||DenseSVM is currently only tested on gcc under 5.2 and 6.3|
+|[Fast Fast-Hadamard Transform](https://github.com/dnbaker/FFHT)|Out-of-place fork of FHT by https://falconn-lib.org/||
+
--- gfrp/include/tx.h
+++ gfrp/include/tx.h
@@ -14,6 +14,7 @@ namespace gfrp { namespace tx {
 #define get_at(val, ind) (val & (1uL << ind))
 #define get_bool(val, ind) (!!get_at(val, ind))
 
+#define process_el(el, rval, c) do {el = vals[get_bool(rval, c++)];if(c & 64) rval = rs(), c = 0;} while(0)
 
 // Multiplies each entry by a Rademacher-distributed random variable.
 template<template<typename, bool> typename MatrixKind, typename FloatType, bool StorageType>
@@ -21,42 +22,50 @@ void rademacher(MatrixKind<FloatType, StorageType> &mat, rng::RandTwister &rs) {
     static const FloatType vals[]{-1, 1};
     auto rval(rs());
     char c(0);
-    if constexpr(StorageType == blaze::rowMajor) {
-        for(std::size_t i(0), e(mat.rows()); i < e; ++i) {
-            auto mrow(row(mat, i));
-            for(auto &el: mrow) {
-                el *= vals[get_bool(rval, c++)];
-                if(c & 64) rval = rs(), c = 0;
+    if constexpr(blaze::IsMatrix<MatrixKind<FloatType, StorageType>>::value) {
+        if constexpr(StorageType == blaze::rowMajor) {
+            for(std::size_t i(0), e(mat.rows()); i < e; ++i) {
+                auto mrow(row(mat, i));
+                for(auto &el: mrow) {
+                    process_el(el, rval, c);
+                }
+            }
+        } else {
+            for(std::size_t i(0), e(mat.rows()); i < e; ++i) {
+                auto mcol(column(mat, i));
+                for(auto &el: mcol) {
+                    process_el(el, rval, c);
+                }
             }
         }
     } else {
-        for(std::size_t i(0), e(mat.rows()); i < e; ++i) {
-            auto mcol(column(mat, i));
-            for(auto &el: mcol) {
-                el *= vals[get_bool(rval, c++)];
-                if(c & 64) rval = rs(), c = 0;
-            }
-        }
+        for(auto &el: mat) process_el(el, rval, c);
     }
 }
 
-template<typename VectorKind>
-void rademacher(VectorKind &vec, rng::RandTwister &rs) {
-    using FloatType = typename VectorKind::ElementType;
-    static const FloatType vals[]{-1, 1};
-    auto rval(rs());
-    char c(0);
-    for(auto &el: vec) {
-        el *= vals[get_bool(rval, c++)];
-        if(c & 64) rval = rs(), c = 0;
-    }
+template<typename MatrixKind>
+MatrixKind make_rademacher(std::size_t n, rng::RandTwister &rs) {
+    MatrixKind ret(n, n);
+    rademacher(ret, rs);
+    return ret;
 }
+
+template<typename VectorKind>
+VectorKind make_rademacher(std::size_t n, rng::RandTwister &rs) {
+    VectorKind ret(n);
+    rademacher(ret, rs);
+    return ret;
+}
+
 
 template<template<typename, bool> typename MatrixKind, typename FloatType, bool StorageType>
 void rademacher(MatrixKind<FloatType, StorageType> &mat) {
     rademacher(mat, rng::random_twist);
 }
 
+#undef process_el
+#undef get_at
+#undef get_bool
 
 }} //namespace gfrp::tx
 
