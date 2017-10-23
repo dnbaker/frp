1d11dbe41c0df2616d4289d08842751eaa117d51
dnbaker <dnb@cs.jhu.edu>
2017-10-23 13:06:06 -0400

    Add notes for DCT/FWHT

--- include/gfrp/compactrad.h
+++ include/gfrp/compactrad.h
@@ -5,11 +5,39 @@
 
 namespace gfrp { namespace dist {
 
+/*
+// From https://arxiv.org/pdf/1702.08159.pdf
+
+FHT!!!
+https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm#Pseudocode
+http://fourier.eng.hmc.edu/e161/lectures/wht/node4.html
+Sliding windows http://www.ee.cuhk.edu.hk/~wlouyang/FWHT.htm
+
+Presentation http://c.csie.org/~itct/slide/DCT_larry.pdf
+
+Using renoramlization makes it orthogonal, which is GOOD.
+some authors further multiply the X0 term by 1/√2 and multiply the resulting matrix by an overall scale factor of 2 N {\displaystyle {\sqrt {\tfrac {2}{N}}}} {\displaystyle {\sqrt {\tfrac {2}{N}}}} (see below for the corresponding change in DCT-III). This makes the DCT-II matrix orthogonal, but breaks the direct correspondence with a real-even DFT of half-shifted input.
+
+
+Fast DCT https://unix4lyfe.org/dct-1d/
+http://ieeexplore.ieee.org/document/558495/
+
+
+
+F2F
+proceeds with vectorized sums and subtractions iteratively for the first n/2^k 
+positions (where n is the length of the input vector and k is the iteration starting from 1)
+computing the intermediate operations of the Cooley-Tukey algorithm till a small Hadamard
+routine that fits in cache.  Then the algorithm continues in the same way but starting from
+the  smallest  length  and  doubling  on  each  iteration  the  input  dimension  until  the  whole
+FWHT is done in-place.
+*/
+
 template<typename T=uint64_t, typename FloatType=FLOAT_TYPE, typename=std::enable_if_t<std::is_floating_point<FloatType>::value>>
 class CompactRademacher {
     size_t n_, m_;
     T *data_;
-    const FloatType values_[2];
+    static constexpr FloatType values_[2] {1, -1};
     static constexpr size_t NBITS = sizeof(T) * CHAR_BIT;
     static constexpr size_t SHIFT = log2_64(NBITS);
     static constexpr size_t BITMASK = NBITS - 1;
@@ -18,7 +46,7 @@ class CompactRademacher {
     using container_type = T;
     using size_type = size_t;
 public:
-    CompactRademacher(size_t n=0): n_{n}, m_{n}, data_(static_cast<T *>(std::malloc((sizeof(T) * n) >> SHIFT))), values_{-1, 1} {
+    CompactRademacher(size_t n=0): n_{n}, m_{n}, data_(static_cast<T *>(std::malloc((sizeof(T) * n) >> SHIFT))) {
         if(n & (n - 1)) {
             std::fprintf(stderr, "Warning: n is not a power of 2. This is a surprise. (%zu)\n", n);
         }
@@ -47,7 +75,7 @@ public:
             data_ = tmp;
         }
     }
-    FloatType operator[](size_type idx) const {return values_[!!(data_[(idx >> SHIFT)] & (static_cast<T>(1) << (idx & BITMASK)))] ;}
+    FloatType operator[](size_type idx) const {return values_[!(data_[(idx >> SHIFT)] & (static_cast<T>(1) << (idx & BITMASK)))] ;}
     ~CompactRademacher(){
         std::free(data_);
     }
