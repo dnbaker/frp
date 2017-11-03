0f0e5dbd88554c7befb4decff73e46376dea8da7
dnbaker <dnb@cs.jhu.edu>
2017-11-03 09:56:55 -0400

    Work on stuff.

--- src/fhtest.cpp
+++ src/fhtest.cpp
@@ -11,14 +11,16 @@ int main(int argc, char *argv[]) {
     std::size_t size(argc <= 1 ? 1 << 16: std::strtoull(argv[1], 0, 10)),
                 niter(argc <= 2 ? 1000: std::strtoull(argv[2], 0, 10));
     size = roundup64(size);
-    blaze::DynamicVector<double> dps(size);
+    blaze::DynamicVector<float> dps(size);
+    blaze::DynamicVector<float> dpsout(size);
+    float *a(&dps[0]), *b(&dpsout[0]);
     aes::AesCtr aes(0);
-    unormd<double> vals;
+    unormd<float> vals;
     for(auto &el: dps) el = vals(aes);
     std::cerr << "Sum: " << gfrp::sum(dps) << '\n';
     gfrp::fht(dps);
     std::cerr << "Sum: " << gfrp::sum(dps) << '\n';
-    blaze::DynamicVector<double> sizes(niter);
+    blaze::DynamicVector<float> sizes(niter);
     for(auto &el: sizes) {
         fht(&dps[0], log2_64(size));
         dps *= 1./std::sqrt(size);
@@ -26,14 +28,20 @@ int main(int argc, char *argv[]) {
         el = gfrp::sum(dps);
     }
     std::cerr << sizes << '\n';
-    fft::FFTWDispatcher<float> disp(size);
-    disp.make_plan(&dps[0], &dps[0]);
-    disp.run(&dps[0], &dps[0]);
+    std::cerr << "now fft\n";
+    auto fplan(fftwf_plan_r2r_1d((int)size, a, b, (fftwf_r2r_kind)fft::REDFT10, FFTW_MEASURE));
+    fftwf_execute(fplan);
+    std::cerr << dps << '\n';
+    std::cerr << dpsout << '\n';
     for(size_t i(0); i < size; ++i) {
-        disp.run(&dps[0], &dps[0]);
+        std::swap(a, b);
+        fftwf_execute(fplan);
+        // Run
         std::cerr << dps << '\n';
+        std::cerr << dpsout << '\n';
         sizes[i] = gfrp::sum(dps);
         std::cerr << "ratio: " << sizes[i] / sizes[i == 0 ? i : i - 1] << '\n';
     }
     std::cerr << sizes;
+    fftwf_destroy_plan(fplan);
 }
