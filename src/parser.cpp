7c9edee83b9f13d64415d0149ea0e004b09a14cd
dnbaker <dnb@cs.jhu.edu>
2017-11-10 15:58:15 -0500

    Progress on type-generic read chunker.

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,6 +1,8 @@
 #include "gfrp/parser.h"
+using namespace gfrp;
 
 int main(int argc, char *argv[]) {
-    IOChunker<FILE *> ic(argc > 1 ? argv[1]: "/dev/urandom", "rb");
+    ChunkedLineReader<FILE *> ic(argc > 1 ? argv[1]: "/dev/urandom", "rb");
+    ChunkedLineReader<gzFile> gzic(argc > 1 ? argv[1]: "/dev/urandom", "rb");
     
 }
