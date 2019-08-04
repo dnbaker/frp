24ff3cf8cf8b428afbf224c05c2c82724c2ea28b
Daniel Nephi Baker <daniel.n.baker@ibm.com>
2019-08-04 03:15:59 -0700

    Save.

--- src/dcitest.cpp
+++ src/dcitest.cpp
@@ -4,19 +4,20 @@
 using namespace frp;
 using namespace dci;
 int main() {
-    int nd = 40;
+    int nd = 40, npoints = 1000;
     DCI<blaze::DynamicVector<float>> dci(20, 120, nd);
     //DCI<blaze::DynamicVector<float>> dci2(10, 4, nd, 1e-5, true);
     std::cerr << "made dci\n";
     std::vector<blaze::DynamicVector<float>> ls;
     std::mt19937_64 mt;
-    for(size_t i = 0; i < 100; ++i) {
+    for(ssize_t i = 0; i < npoints; ++i) {
         ls.emplace_back(nd);
         for(auto &x: ls.back())
             x = std::ldexp(double(mt()), 64);
     }
     for(const auto &v: ls)
         dci.add_item(v);//, dci2.add_item(v);
-    dci.query(ls[0], 3);
+    auto topn = dci.query(ls[0], 3);
+    std::fprintf(stderr, "topn: %zu is n\n", topn.size());
     std::cerr << "added item to dci\n";
 }
